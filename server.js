0800eb07d656bfdec5772659f2cd395674266fe3
soyket <soyket.handymama@gmail.com>
2018-08-17 22:10:03 +0600

    add todos 2

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const users = require('./api/routes/user');
+
+// connect to mongoose
+mongoose.connect('mongodb://soyket:handymama123@ds151452.mlab.com:51452/nodekb', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('DB Active...');
+});
 
 const app = express();
 app.use(morgan('dev'));
